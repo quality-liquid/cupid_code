f92d8885b396fbb17e9c48e52cb580d62a7e7071
Emma <a02324881@usu.edu>
2024-02-20 11:37:09 -0700

    Working on setting up

---
+++ Dev/README.md
@@ -0,0 +1,6 @@
+# Django + Vue
+
+## Django
+
+
+## Vue
---
+++ Dev/client/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./src/main.js"
+    },
+    outDir: "../<server>/core/static/core"
+  },
+  base: "/static"
+})
