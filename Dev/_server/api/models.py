c16e408f95382a1cd9003c23e9de4206fc4da163
Nate McKenzie <nate6264@gmail.com>
2024-02-21 09:01:41 -0700

    One-for-one paste of design psuedo

--- Dev/_server/api/models.py
+++ Dev/_server/api/models.py
@@ -1,3 +1,100 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        DATER = 'Dater'
+        CUPID = 'Cupid'
+        MANAGER = 'Manager'
+
+    role = models.CharField(choices=Role.choices, max_length=7)
+
+class Dater(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    phone_number = models.CharField(max_length=10)
+    budget = models.DecimalField(max_digits=10, decimal_places=2)
+    communication_preference = models.IntegerField()
+    profile_picture = models.ImageField()
+    description = models.TextField()
+    dating_strengths = models.TextField()
+    dating_weaknesses = models.TextField()
+    interests = models.TextField()
+    past = models.TextField()
+    nerd_type = models.TextField()
+    relationship_goals = models.TextField()
+    ai_degree = models.TextField()
+    cupid_cash_balance = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.TextField()
+    average_rating = models.DecimalField(max_digits=10, decimal_places=2)
+    suspended = models.BooleanField()
+
+class Cupid(models.Model):
+    class Status(models.IntegerChoices):
+        OFFLINE = 0
+        GIGGING = 1
+        AVAILABLE = 2
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    accepting_gigs = models.BooleanField()
+    gigs_completed = models.IntegerField()
+    gigs_failed = models.IntegerField()
+    payment = models.TextField()
+    status = models.IntegerField(choices=Status.choices)
+    cupid_cash_balance = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.TextField()
+    average_rating = models.DecimalField(max_digits=10, decimal_places=2)
+    
+class Message(models.Model):
+    owner = models.ForeignKey(User, primary_key=True, on_delete=models.CASCADE)
+    text = models.TextField()
+    from_ai = models.BooleanField()
+
+class Gig(models.Model):
+    class Status(models.IntegerChoices):
+        UNCLAIMED = 0
+        CLAIMED = 1
+        COMPLETE = 2
+        DROPPED = 2
+
+    dater = models.ForeignKey(Dater, on_delete=models.CASCADE)
+    cupid = models.ForeignKey(Cupid, on_delete=models.CASCADE)
+    quest = models.OneToOneField(Quest, on_delete=models.CASCADE)
+    status = models.IntegerField(chioces=Status.choices)
+    date_time_of_request = models.DateTimeField()
+    date_time_of_claim = models.DateTimeField()
+    date_time_of_completion = models.DateTimeField()
+
+class Quest(models.Model):
+    gig = models.OneToOneField(Gig, on_delete=models.CASCADE, primary_key = True)
+    budget = models.DecimalField(max_digits=10, decimal_places=2)
+    items_requested = models.TextField()
+    pickup_location = models.TextField()
+    
+class Date(models.Model):
+    dater = models.ForeignKey(Dater, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+    location = models.TextField()
+    description = models.TextField()
+    status = models.TextField()
+    budget = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Feedback(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    gig = models.ForeignKey(Gig, on_delete=models.CASCADE)
+    message = models.TextField()
+    star_rating = models.IntegerField()
+    date_time = models.DateTimeField()
+    
+class PaymentCard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    card_number = models.TextField()
+    cvv = models.TextField()
+    expiration = models.TextField()
+
+class BankAccount(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    routing_number = models.TextField()
+    account_number = models.TextField()
+
