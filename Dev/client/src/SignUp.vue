14e19fb5de3ac8bd565af18bfec2d0828a750fab
ekfsdpw <a02242376@aggies.usu.edu>
2024-02-23 09:41:38 -0700

    Started work on Vue Pages: Login and Create Account

---
+++ Dev/client/src/SignUp.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <img src="../assets/cupid_logo.png" alt="Cupid Code Logo">
+        <h1>Create Your Account!</h1>
+        <form action="/registration/sign_in/">
+            <label class="form_input">
+                Account Type
+                <br>
+                
+                <input type="radio" id="cupid" name="accountType" value="Cupid">
+                <label for="cupid">Cupid</label>
+                <input type="radio" id="dater" name="accountType" value="Cupid">
+                <label for="dater">Dater</label>
+                
+                <br>
+                <button>Create Account</button>
+            </label>
+        </form>
+
+        <a href="Sign Up">Create Account</a>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
