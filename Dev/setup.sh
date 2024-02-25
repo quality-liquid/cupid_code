75c38d4e47ec19afbc081b9f6f5dcadd6d9d597e
Nate Stott <nate.stott@pm.me>
2024-02-25 10:46:49 -0800

    update scripts so that python or python3 works

--- Dev/setup.sh
+++ Dev/setup.sh
@@ -14,8 +14,18 @@ poetry shell
 
 # Apply Django migrations
 cd _server || exit
-python manage.py makemigrations
-python manage.py migrate
+# Check if python3 command is available
+if command -v python3 &>/dev/null; then
+    python3 manage.py makemigrations
+    python3 manage.py migrate
+# Check if python command is available
+elif command -v python &>/dev/null; then
+    python manage.py makemigrations
+    python manage.py migrate
+else
+    echo "Python interpreter not found. Please install Python."
+    exit 1
+fi
 
 # Navigate to Vue client directory
 cd ../client || exit
--- Dev/start.sh
+++ Dev/start.sh
@@ -2,7 +2,16 @@
 
 # Run Django server
 echo "Starting Django server..."
-python manage.py runserver
+# Check if python3 command is available
+if command -v python3 &>/dev/null; then
+    python3 manage.py runserver
+# Check if python command is available
+elif command -v python &>/dev/null; then
+    python manage.py runserver
+else
+    echo "Python interpreter not found. Please install Python."
+    exit 1
+fi
 
 # Run Vue development server
 echo "Starting Vue development server..."
