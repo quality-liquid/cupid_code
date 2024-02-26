91af7ee09c1f2427dc591919ec9cc2e4b7980cef
Emma (Laptop) <a02324881@usu.edu>
2024-02-26 08:50:46 -0700

    Worked on stuff

--- Dev/client/src/components/Welcome.vue
+++ Dev/client/src/components/Welcome.vue
@@ -1,28 +1,7 @@
 <script>
-import {ref, computed} from 'vue'
-import { makeRequest } from './utils/make_request';
-// Import login components
-const currPath = ref(window.location.hash)
-
-const routes = {
-    '#/login': Login,
-    '#/signup': Signup,
-}
-
-eventListener("hashchange", () => {
-  currPath.value = window.location.hash
-}) // This will run everytime it's changed to ensure it's correct.
-
-const currView = computed(() => {
-  return routes[currPath.value.slice(1) || '/']
-}) 
-
 </script>
 
 <template>
-  <nav v-for="route in routes" :key="route">
-    <a href={{ route }}> {{ chosenRoute[route] }}</a>
-  </nav>
   <component :is="currView" />
   
   <div v-if="currView === '/'">
--- Dev/client/src/main.js
+++ Dev/client/src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import Welcome from './components/Welcome.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      { path: '/', component: Welcome },
+      { path: '/register', component: RegisterPage },
+      { path: '/login', component: LoginPage },
+      { path: '/dashboard', component: DashboardPage },
+    ],
+  });
+
 
 createApp(App).mount('#app')
