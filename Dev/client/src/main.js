e1c79044fd37410d62048ccc352dd63aaff47864
Emma (Laptop) <a02324881@usu.edu>
2024-02-26 09:03:36 -0700

    Wrote bases for login stuff; async functions, welcome pg, app vue routers

--- Dev/client/src/main.js
+++ Dev/client/src/main.js
@@ -2,17 +2,20 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import Welcome from './components/Welcome.vue';
+import Login from './components/Login.vue';
+import SignUp from './components/SignUp.vue'
+import UniversalHome from './components/UniversalHome.vue'
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
       { path: '/', component: Welcome },
-      { path: '/register', component: RegisterPage },
-      { path: '/login', component: LoginPage },
-      { path: '/dashboard', component: DashboardPage },
+      { path: '/register', component: Signup },
+      { path: '/login', component: Login },
+      { path: '/home', component: UniversalHome },
     ],
-  });
+});
 
 
 createApp(App).mount('#app')
