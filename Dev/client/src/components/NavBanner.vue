ab861461750ad73cb71ad1b706a5c5a2f74f3496
Brighton E <brighton.ellis@gmail.com>
2024-02-27 14:18:27 -0700

    skeleton nav bar component created.

---
+++ Dev/client/src/components/NavBanner.vue
@@ -0,0 +1,42 @@
+<!-- html render here 
+      takes values
+-->
+<template id="topBanner">
+  <nav>
+    <figure @click="goBack" >{{ backIcon }}</figure>
+    <figure>{{ searchIcon }}</figure>
+    <figure @click="userAccount">{{ accountIcon }}</figure>
+  </nav>
+</template>
+
+
+
+
+
+  <!-- variables + functions here -->
+  <script>
+    console.log("Header Started")
+  
+    const myVueComp = new Vue({
+      el: "topBanner",
+      data: {
+        backIcon: "Back arrow",
+        searchIcon: "magnifying glass",
+        userInfo: "money/rating here",
+        accountIcon: "account icon",
+      },
+      methods: {
+        goBack(){
+          // redirect to user home page
+        },
+        userAccount(){
+          // redirect to user account page
+        },
+        searchIcon(){
+          // allow for search capability
+        }
+      }
+  
+    })
+  
+  </script>
