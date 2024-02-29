54d4d66b82e5905582976e06c3914d7828be12a3
Emma (Laptop) <a02324881@usu.edu>
2024-02-29 12:16:44 -0700

    Workin on signup page unique info

--- Dev/client/src/components/SignUp.vue
+++ Dev/client/src/components/SignUp.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup>
 import { makeRequest } from '../utils/make_request.js';
 import {ref} from 'vue';
 
@@ -23,15 +23,13 @@ async function register() {
     // Redirect to dashboard
 }
 
-
-export default {
-  name: 'SignUp'
-};
 </script>
 
 <template>
+    <div class="image">
+        <img :src="'/get_img/'" alt="Cupid Code Logo" width="200" height="200">
+    </div>
     <div class="container">
-        <img :src="'/get_img/'"  alt="Cupid Code Logo">
         <h1>Create Your Account!</h1>
         <form class="form">
             <label class="input_detail" for="email">
@@ -52,7 +50,7 @@ export default {
             </label>
             <label class="input_detail" for="image">
                 Profile Picture
-                <input type="image" :value="image" @change="(e) => image = e.target.value"/>
+                <input type="image" name="image" :value="image" @change="(e) => image = e.target.value"/>
             </label>
             <label class="input_detail" for="address">
                 Address
@@ -60,14 +58,27 @@ export default {
             </label>
             <div class="radios">
                 Account Type
-                <label class="input_detail" for="cupid">
+                <label class="radio_detail" for="cupid">
                     Cupid
                     <input type="radio" id="cupid" name="accountType" :value="accType" />
                 </label>
-                <label class="input_detail" for="dater">
+                <label class="radio_detail" for="dater">
                     Dater
                     <input type="radio" id="dater" name="accountType" :value="accType" />
                 </label>
+            </div>
+            <div v-if="accType.toLowerCase === 'dater'">
+                <label>
+                    Dating Weaknesses
+                    <textarea></textarea>
+                </label>
+                <label>
+                    Dating Strengths
+                    <textarea></textarea>
+                </label>
+            </div>
+            <div v-else-if="accType.toLowerCase === 'cupid'">
+
             </div>
 
             <button class="button" @click="register">Create Account</button>
@@ -76,5 +87,74 @@ export default {
 </template>
 
 <style scoped>
+    .image {
+        display: flex;
+        margin-top: 50px;
+        border-radius: 16px;
+    }
+    .container {
+        display: flex;
+        margin-top: 20px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: var(--secondary-blue);
+        border-radius: 16px;
+        color: white;
+    }
 
+    .button {
+        background-color: var(--primary-red);
+        border-radius: 10px;
+        color: white;
+        border: none;
+        border-radius: 4px;
+    }
+
+    .form {
+        display: flex;
+        flex-flow: column wrap;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .input_detail {
+        margin: 4px;
+        text-align: center;
+    }
+
+    .radios {
+        display: flex;
+        flex-flow: column wrap;
+    }
+
+    input {
+        display: flex;
+        flex-direction: column;
+        border: none;
+        padding: 16px;
+        border-radius: 8px;
+        margin: 8px;
+    }
+
+    input[name="accountType"] {
+        display: flex;
+        border: none;
+        color: var(--secondary-red);
+    }
+
+    input[name="image"] {
+        display: flex;
+    }
+
+    .button {
+        margin: 10px;
+        padding: 16px;
+        border: none;
+        border-radius: 8px;
+    }
+
+    .error {
+        border: 4px var(--secondary-red) solid;
+    }
 </style>
