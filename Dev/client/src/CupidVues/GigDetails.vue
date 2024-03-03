649f8cedbfbe3647edddcf98acb4dfde0de1a43a
ekfsdpw <a02242376@aggies.usu.edu>
2024-03-03 14:17:17 -0700

    Added dynamic addition of gigs to homepage and a very basic gig details page

---
+++ Dev/client/src/CupidVues/GigDetails.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <h1><!-- {{ gig.location }} --></h1>
+        <p>Pickup: <!-- {{ gig.pickup }} --></p>
+        <p>Deliver: <!-- {{ gig.deliver }} --></p>
+        <p>Status: <!-- {{ gig.active ? 'Active' : 'Inactive' }} --></p>
+    </div>
+</template>
+
+<style>
+</style>
