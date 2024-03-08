58930664f52d3e5afbc48e7ec1daca3465f2e8a2
ekfsdpw <a02242376@aggies.usu.edu>
2024-03-08 08:38:55 -0700

    Added Nav Bars

--- Dev/client/src/CupidVues/CupidHome.vue
+++ Dev/client/src/CupidVues/CupidHome.vue
@@ -1,13 +1,33 @@
 <script setup>
-    import { makeRequest } from '../utils/make_request.js';
-    import { ref, computed } from 'vue';
-
-    
+    const user_id  = parseInt(window.location.hash.split('/')[3]) //Gets the id from the router
+    // Open and closes drawer w/ shorthand
+    function openDrawer() {
+        const element = document.getElementById('navbar')
+        if (element.className === 'navbar') {
+            element.className = 'navbar opened'
+        }
+        else {
+            element.className = 'navbar'
+        }
+    }
+    // Logout function
+    async function logout() {
+        const result = await makeRequest(`/logout/`)
+        router.push('/')
+    }
 </script>
 
 <template>
-    <nav>
-        Wow, a nav bar
+    <nav class="nav homenav">
+        <button @click="openDrawer" class="icon-button">
+            <img :src="'/get_menu/'" alt="Menu Open icon" class="icon">
+        </button>
+        <!-- This will be the profile picture when setup -->
+        <img :src="'/get_menu/'" alt="Profile Picture" class="icon">
+        <div id="navbar" class="navbar">
+            <router-link class="link" :to="{name: 'CupidHome', params: {id: user_id}}"> Home </router-link>
+            <button class="logout" @click="logout"> Logout </button>
+        </div>
     </nav>
 
     <div class="body">
