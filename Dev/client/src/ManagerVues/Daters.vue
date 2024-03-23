b2a6db2f8f9dcfb5c89a964d4795d3bf2cbce3be
Emma <a02324881@usu.edu>
2024-03-23 15:36:38 -0600

    Adjusted pages to use the same syntax as the rest of the pages - no reason to make objects and methods since the backend basically does that already

--- Dev/client/src/ManagerVues/Daters.vue
+++ Dev/client/src/ManagerVues/Daters.vue
@@ -9,12 +9,12 @@
     <section class="userListItem">
       <nav>
         <figure>icon</figure>
-        <p>{{ dater.name }}</p>
+        <p>{{ dater.user.first_name }}</p>
       </nav>
       <article>
-        <p>Rating: {{ dater.rating }}</p>
+        <p>Rating: {{ dater.rating_sum }}</p>
         <p>Location: {{ dater.location }}</p>
-        <p>Completed Gigs: {{ dater.completedGigs }}</p>
+        <p>Completed Gigs: {{ dater.completed_gigs }}</p>
         <button>Suspend/Unsuspend</button>
       </article>      
     </section>
@@ -25,30 +25,40 @@
 
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import { makeRequest } from '../utils/make_request';
 
-  let daters = ref([{ }])
-
-  const daterComponent = new VueElement({
-    
-    element: "#daterstuffs",
-    data: {
-      name: "",
+const daters = ref([{ }])
+/*
+Bad syntax. These 
+const daterComponent = new VueElement({
+  
+  element: "#daterstuffs",
+  data: {
+    name: "",
+  },
+  methods: {
+    getDaterName(){
+      this.name
     },
-    methods: {
-      getDaterName(){
-        this.name
-      },
-      getDaterRating() {
-        this.rating
-      },
-      getDaterLocation(){
-        this.location
-      },
-      getDaterTotalDates(){
-        this.totalDates
-      }
+    getDaterRating() {
+      this.rating
+    },
+    getDaterLocation(){
+      this.location
+    },
+    getDaterTotalDates(){
+      this.totalDates
     }
-  })
+  }
+})
+*/
+
+async function getDaters() {
+  const res = await makeRequest('/api/manager/daters')
+  daters.value = res.daters
+}
+  
 
 </script>
 
--- Dev/client/src/ManagerVues/ManagerHome.vue
+++ Dev/client/src/ManagerVues/ManagerHome.vue
@@ -7,14 +7,17 @@
 
   <figure>Graph</figure>
 
-  <figure>{{ daters.total }} Users</figure>
-  <figure>{{ cupids.total }} Cupids stats</figure>
-  <figure>{{ cupids.currActiveTotal }} Active Cupids</figure>
+  <figure>{{ daters }} Users</figure>
+  <figure>{{ cupids }} Cupids stats</figure>
+  <figure>{{ active_cupids }} Active Cupids</figure>
+  <figure>{{ active_daters }} Active Daters</figure>
   <figure>Other Info</figure>
 
 </template>
 
 <script setup>
+/*
+  Not valid Vue 3 syntax
   const managerComponent = new VueElement({
     el: "#managerPage",
     data: {
@@ -32,6 +35,39 @@
       }
     }
   })
+*/
+
+
+// NOTE: I tried matching the results names to what I can see in the views.py and helper.py 
+//If things are reading null or undefined, just console.log(results) to see what the names are and then change `results.data` to `results.<foundName>`
+import {ref, onMounted} from 'vue'
+import { makeRequest } from '../utils/make_request';
+
+const daters = ref(0) //Fill with a setter type for whatever the backend returns to you 
+const cupids = ref(0)
+const active_cupids = ref(0)
+const active_daters = ref(0)
+
+async function getDatersTotal() {
+  const results = await makeRequest('/api/manager/dater_count')
+  daters.value = results.data
+}
+
+async function getCupidsTotal() {
+  const results = await makeRequest('/api/manager/cupid_count')
+  cupids.value = results.data
+}
+
+async function getCurrActiveTotal() {
+  const cupid_res = await makeRequest('/api/manager/active_cupids')
+  active_cupids.value = cupid_res.data 
+  const dater_res= await makeRequest('/api/manager/active_daters/')
+  active_daters.value = dater_res.data
+}
+
+onMounted(getCupidsTotal)
+onMounted(getDatersTotal)
+onMounted(getCurrActiveTotal)
 </script>
 
 
