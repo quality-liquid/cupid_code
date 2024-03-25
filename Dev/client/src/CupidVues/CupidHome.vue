7ab0da16593cd4ef0caf3221368969b694f0ae0f
Emma (Laptop) <a02324881@usu.edu>
2024-03-25 09:37:18 -0600

    Added navbar

--- Dev/client/src/CupidVues/CupidHome.vue
+++ Dev/client/src/CupidVues/CupidHome.vue
@@ -53,6 +53,9 @@
         <img :src="'/get_temp_pfp/'" alt="Profile Picture" class="icon">
         <div id="navbar" class="navbar">
             <router-link class="link" :to="{name: 'CupidHome', params: {id: user_id}}"> Home </router-link>
+            <router-link class="link" :to="{name: 'CupidDetails', params: {id: user_id}}"> Profile </router-link>
+            <router-link class="link" :to="{name: 'GigDetails', params: {id: user_id}}"> Gig Details </router-link>
+            <router-link class="link" :to="{name: 'GigComplete', params: {id: user_id}}"> Check Completed </router-link>
             <button class="logout" @click="logout"> Logout </button>
         </div>
     </nav>
