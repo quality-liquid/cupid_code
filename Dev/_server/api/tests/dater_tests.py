01bbac3d065b9941adb98757634d018e6ba5a878
Nate Stott <nate.stott@pm.me>
2024-03-28 16:44:02 -0600

    test send chat message

--- Dev/_server/api/tests/dater_tests.py
+++ Dev/_server/api/tests/dater_tests.py
@@ -10,7 +10,7 @@ from Dev._server.api.helpers import *
 
 
 class TestGetAIResponse(APITestCase):
-    
+
     @patch('GPT2Tokenizer.from_pretrained')
     @patch('GPT2LMHeadModel.from_pretrained')
     def good_test(self, mock_tokenizer, mock_model):
@@ -49,11 +49,51 @@ class TestGetAIResponse(APITestCase):
 
 
 class TestSendChatMessage(APITestCase):
-    pass
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.url = reverse('api/send_chat_message')
+        self.view = send_chat_message
+
+    @patch('helpers.get_location_string')
+    @patch('MessageSerializer')
+    @patch('helpers.get_ai_response')
+    def test_good(self, mock_get_location_string, mock_message_serializer, mock_get_ai_response):
+        request = self.factory.post(self.url)
+        force_authenticate(request, user=User.objects.get(username='test'))
+        mock_get_location_string.return_value = "Location"
+        mock_message_serializer.return_value = MagicMock()
+        mock_get_ai_response.return_value = "Response"
+        response = self.view(request)
+        assert response.data == "Response"
+        assert response.status_code == status.HTTP_200_OK
+        mock_get_location_string.assert_called_once()
+        mock_message_serializer.assert_called_once()
+        mock_get_ai_response.assert_called_once()
+
+    @patch('helpers.get_location_string')
+    @patch('MessageSerializer')
+    @patch('helpers.get_ai_response')
+    def test_bad(self, mock_get_location_string, mock_message_serializer, mock_get_ai_response):
+        request = self.factory.post(self.url)
+        force_authenticate(request, user=User.objects.get(username='test'))
+        mock_get_location_string.return_value = "Location"
+        serializer = MagicMock()
+        serializer.is_valid.return_value = False
+        mock_message_serializer.return_value = serializer
+        response = self.view(request)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        mock_get_location_string.assert_called_once()
+        mock_message_serializer.assert_called_once()
+        mock_get_ai_response.assert_not_called()
 
 
 class TestGetFiveMessages(APITestCase):
-    pass
+        
+        def setUp(self):
+            self.factory = APIRequestFactory()
+            self.url = reverse('api/get_five_messages')
+            self.view = get_five_messages
 
 
 class TestCalendar(APITestCase):
