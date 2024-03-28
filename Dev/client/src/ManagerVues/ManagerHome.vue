9acfcc843259a65b3475eea5c4a5caa73f40a00a
Emma <a02324881@usu.edu>
2024-03-28 17:07:57 -0600

    Worked on stlying & adding stuff to manager pgs. Added defaults.md with all default users

--- Dev/client/src/ManagerVues/ManagerHome.vue
+++ Dev/client/src/ManagerVues/ManagerHome.vue
@@ -10,46 +10,82 @@ const active_cupids = ref(0)
 const active_daters = ref(0)
 
 async function getDatersTotal() {
-  const results = await makeRequest('/api/manager/dater_count')
-  daters.value = results.data
+  const results = await makeRequest('/api/manager/dater_count/')
+  daters.value = results.count
 }
 
 async function getCupidsTotal() {
-  const results = await makeRequest('/api/manager/cupid_count')
-  cupids.value = results.data
+  const results = await makeRequest('/api/manager/cupid_count/')
+  cupids.value = results.count
 }
 
 async function getCurrActiveTotal() {
-  const cupid_res = await makeRequest('/api/manager/active_cupids')
+  const cupid_res = await makeRequest('/api/manager/active_cupids/')
   active_cupids.value = cupid_res.data 
   const dater_res= await makeRequest('/api/manager/active_daters/')
   active_daters.value = dater_res.data
 }
 
-onMounted(getCupidsTotal)
-onMounted(getDatersTotal)
-onMounted(getCurrActiveTotal)
+const user_id  = parseInt(window.location.hash.split('/')[3]) //Gets the id from the router
+// Open and closes drawer w/ shorthand
+function openDrawer() {
+  const element = document.getElementById('navbar')
+  if (element.className === 'navbar') {
+    element.className = 'navbar opened'
+  }
+  else {
+    element.className = 'navbar'
+  }
+}
+// Logout function
+async function logout() {
+  const result = await makeRequest(`/logout/`)
+  router.push('/')
+}
+
+onMounted(() => {
+  getCupidsTotal()
+  getDatersTotal()
+  //getCurrActiveTotal()
+})
 </script>
 
 <template>
-  <!-- nav banner component -->
-  <!--  -->
-
-  <article>Daters</article>
-  <article>Cupids</article>
-
-  <figure>Graph</figure>
-
-  <figure>{{ daters }} Users</figure>
-  <figure>{{ cupids }} Cupids stats</figure>
-  <figure>{{ active_cupids }} Active Cupids</figure>
-  <figure>{{ active_daters }} Active Daters</figure>
-  <figure>Other Info</figure>
+  <nav class="nav homenav">
+      <button @click="openDrawer" class="icon-button">
+          <img :src="'/get_menu/'" alt="Menu Open icon" class="icon">
+      </button>
+      <!-- This will be the profile picture when setup -->
+      <img :src="'/get_temp_pfp/'" alt="Profile Picture" class="icon">
+      <div id="navbar" class="navbar">
+          <router-link class="link" :to="{name: 'ManagerHome', params: {id: user_id}}"> Home </router-link>
+          <router-link class="link" :to="{name: 'ManageDaters', params: {id: user_id}}"> See Dater Info </router-link>
+          <router-link class="link" :to="{name: 'ManageCupids', params: {id: user_id}}"> See Cupid Info </router-link>
+          <button class="logout" @click="logout"> Logout </button>
+      </div>
+  </nav>
+  <main class="container">
+    <h3>Revenue Graph (Very Real)</h3>
+    <figure>
+      <img :src="'/get_graph/'" alt="Graph" class="graph" width="300px">
+      <figcaption>Graph of Overall Revenue</figcaption>
+    </figure>
+  
+    <h3>General Stats</h3>
+    <div>
+      <span>{{ daters }} Total Daters</span>
+      <span>{{ cupids }} Total Cupids</span>
+      <span>{{ active_cupids }} Active Cupids</span>
+      <span>{{ active_daters }} Active Daters</span>
+    </div>
+  </main>
 
 </template>
 
 <style scoped>
-
+.container {
+  margin-top: 50px;
+}
 </style>
   
 
