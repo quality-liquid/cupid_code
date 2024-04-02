bcea23b7436175341be853e6fdac9f7cccd33bfc
Nate Stott <nate.stott@pm.me>
2024-04-01 19:13:48 -0600

    fixed bug 2?

--- Dev/_server/api/views.py
+++ Dev/_server/api/views.py
@@ -1145,6 +1145,24 @@ def get_active_daters(request):
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated, IsAdminUser])
+def get_gig_count(request):
+    """
+    A manager can get the number of gigs that are currently active.
+
+    Args:
+        request: Information about the request.
+    Returns:
+        Response:
+            If the number of gigs that are currently active was retrieved successfully, return the gig count and a 200 status code.
+            If the number of gigs that are currently active was not retrieved successfully, return an error message and a 400 status code.
+    """
+    number_of_gigs = Gig.objects.all().count()
+    return Response({'count': number_of_gigs}, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated, IsAdminUser])
 def get_gig_rate(request):
     """
     A manager can get the rate of gigs per hour.
@@ -1160,28 +1178,9 @@ def get_gig_rate(request):
         yesterday = datetime.now() - datetime.timedelta(days=1)
         gigs_from_past_day = Gig.objects.filter(date_time_of_request_range=(yesterday, datetime.now()))
         gig_rate = gigs_from_past_day.count() / 24
-        response = gig_rate.json()
-        return Response(response, status=status.HTTP_200_OK)
+        return Response(data={"gig_rate": gig_rate}, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({'error': e}, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET'])
-@authentication_classes([SessionAuthentication, BasicAuthentication])
-@permission_classes([IsAuthenticated, IsAdminUser])
-def get_gig_count(request):
-    """
-    A manager can get the number of gigs that are currently active.
-
-    Args:
-        request: Information about the request.
-    Returns:
-        Response:
-            If the number of gigs that are currently active was retrieved successfully, return the gig count and a 200 status code.
-            If the number of gigs that are currently active was not retrieved successfully, return an error message and a 400 status code.
-    """
-    number_of_gigs = Gig.objects.all().count()
-    return Response({'count': number_of_gigs}, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
@@ -1204,11 +1203,8 @@ def get_gig_drop_rate(request):
         gigs_from_past_day = Gig.objects.filter(date_time_of_request_range=(yesterday, datetime.now()))
         for gig in gigs_from_past_day:
             number_of_drops += gig.dropped_count
-
         drop_rate = number_of_drops / 24
-        response = drop_rate.json()
-        return Response(response, status=status.HTTP_200_OK)
-
+        return Response(data={"drop_rate": drop_rate}, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({'error': e}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -1231,8 +1227,7 @@ def get_gig_complete_rate(request):
         number_of_completed_gigs = Gig.objects.filter(status=2).count()
         number_of_gigs = Gig.objects.all().count()
         gig_complete_rate = number_of_completed_gigs / number_of_gigs
-        response = gig_complete_rate.json()
-        return Response(response, status=status.HTTP_200_OK)
+        return Response(data={"gig_complete_rate": gig_complete_rate}, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({'error': e}, status=status.HTTP_400_BAD_REQUEST)
 
