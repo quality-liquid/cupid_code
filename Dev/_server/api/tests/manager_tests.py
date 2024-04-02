6913982a9d15a44efab31787eec1027490856fad
Daniel <barfussdaniel3@gmail.com>
2024-04-01 18:54:50 -0600

    Pausing on user tests to figure out user_expand test. Started on manager tests, going great, I think.

--- Dev/_server/api/tests/manager_tests.py
+++ Dev/_server/api/tests/manager_tests.py
@@ -7,13 +7,109 @@ from rest_framework.test import APITestCase
 from Dev._server.api.models import *
 from Dev._server.api.views import *
 
+import views
+from serializers import (
+    UserSerializer,
+    DaterSerializer,
+    CupidSerializer,
+    ManagerSerializer,
+    MessageSerializer,
+    GigSerializer,
+    DateSerializer,
+    FeedbackSerializer,
+    PaymentCardSerializer,
+    BankAccountSerializer,
+    QuestSerializer,
+)
+
 
 class TestGetCupids(APITestCase):
-    pass
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.url = reverse('api/get_cupids')
+        self.view = views.get_cupids()
 
+    @patch("Cupid.objects.all")
+    @patch("CupidSerializer")
+    def good_test(self, mock_all_cupids, mock_serializer):
+        mock_all_cupids.return_value = MagicMock()
+        mock_serializer.return_value = MagicMock()
+        request = self.factory.post(self.url)
+        
+        request.data = {'test': 'test'}
+        response = self.view(request)
+        assert response.status_code == status.HTTP_200_OK
+        mock_all_cupids.assert_called_once()
+        mock_serializer.assert_called_once()
+
+    @patch("Cupid.objects.all")
+    @patch("CupidSerializer")
+    def bad_test(self, mock_all_cupids, mock_serializer):
+        # Test 1: Testing if Cupid.objects.all gives back nothing
+        mock_all_cupids.return_value = None
+        mock_serializer.return_value = MagicMock()
+        request = self.factory.post(self.url)
+        
+        request.data = {'test': 'test'}
+        response = self.view(request)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        mock_all_cupids.assert_called_once()
+        mock_serializer.assert_not_called()    
+
+        # Test 2: Testing if CupidSerializer gives back nothing
+        mock_all_cupids.return_value = MagicMock()
+        mock_serializer.return_value = None
+        request = self.factory.post(self.url)
+        
+        request.data = {'test': 'test'}
+        response = self.view(request)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        mock_all_cupids.assert_called_once()
+        mock_serializer.assert_called_once() 
 
 class TestGetDaters(APITestCase):
-    pass
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.url = reverse('api/get_daters')
+        self.view = views.get_daters()
+
+    @patch("Dater.objects.all")
+    @patch("DaterSerializer")
+    def good_test(self, mock_all_daters, mock_serializer):
+        mock_all_daters.return_value = MagicMock()
+        mock_serializer.return_value = MagicMock()
+        request = self.factory.post(self.url)
+        
+        request.data = {'test': 'test'}
+        response = self.view(request)
+        assert response.status_code == status.HTTP_200_OK
+        mock_all_daters.assert_called_once()
+        mock_serializer.assert_called_once()
+
+    @patch("Dater.objects.all")
+    @patch("DaterSerializer")
+    def bad_test(self, mock_all_daters, mock_serializer):
+        # Test 1: Testing if Dater.objects.all gives back nothing
+        mock_all_daters.return_value = None
+        mock_serializer.return_value = MagicMock()
+        request = self.factory.post(self.url)
+        
+        request.data = {'test': 'test'}
+        response = self.view(request)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        mock_all_daters.assert_called_once()
+        mock_serializer.assert_not_called()    
+
+        # Test 2: Testing if DaterSerializer gives back nothing
+        mock_all_daters.return_value = MagicMock()
+        mock_serializer.return_value = None
+        request = self.factory.post(self.url)
+        
+        request.data = {'test': 'test'}
+        response = self.view(request)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        mock_all_daters.assert_called_once()
+        mock_serializer.assert_called_once() 
 
 
 class TestGetDaterCount(APITestCase):
--- Dev/_server/api/tests/user_tests.py
+++ Dev/_server/api/tests/user_tests.py
@@ -1,13 +1,33 @@
+from rest_framework.test import APIRequestFactory, force_authenticate
+from unittest.mock import patch, MagicMock
 from django.urls import reverse
-from rest_framework.test import APIRequestFactory, APITestCase
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.test import APITestCase
 from Dev._server.api.views import *
 
+from serializers import (
+    UserSerializer,
+    DaterSerializer,
+    CupidSerializer,
+    ManagerSerializer,
+    MessageSerializer,
+    GigSerializer,
+    DateSerializer,
+    FeedbackSerializer,
+    PaymentCardSerializer,
+    BankAccountSerializer,
+    QuestSerializer,
+)
+from models import (User, Dater, Cupid, Gig, Quest, Message, Date, Feedback, PaymentCard, BankAccount)
+import helpers
+import views
 
 class TestCreateUser(APITestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
         self.url = reverse('api/create_user')
-        self.view = create_user
+        self.view = views.create_user
 
     @patch("UserSerializer")
     @patch("DaterSerializer")
@@ -51,21 +71,21 @@ class TestSignIn(APITestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
         self.url = reverse('api/sign_in')
-        self.view = sign_in
+        self.view = views.sign_in
         self.user = MagicMock()
 
     @patch('User.objects.get')
     @patch('helpers.update_location')
     def good_test(self, mock_get_user, mock_update_location):
         mock = MagicMock()
-        # TODO Pretty sure I need to tie in 'username' somehow. Test that an incorrect username yields bad login
         mock.user_id = 1
+        mock.username = "Test"
         mock_get_user.return_value = mock
         mock_update_location.return_value = None
         request = self.factory.post(self.url)
         force_authenticate(request, user=self.user)
 
-        request.data = {'user_id': 1, 'location': 'Test', 'role': 'dater', 'phone_number': '1123456789'}
+        request.data = {'user_id': 1, 'location': 'Test', 'role': 'dater', 'phone_number': '1123456789', 'username': 'Test'}
         response = self.view(request)
         assert response.status_code == status.HTTP_200_OK
         mock_get_user.assert_called_once()
@@ -76,28 +96,115 @@ class TestSignIn(APITestCase):
     def bad_test(self, mock_get_user, mock_update_location):
         mock = MagicMock()
         mock.user_id = 1
+        mock.username = None
         mock_get_user.return_value = mock
         mock_update_location.return_value = None
         request = self.factory.post(self.url)
         force_authenticate(request, user=self.user)
 
-        request.data = {'user_id': 1, 'location': 'Test', 'role': 'dater', 'phone_number': '1123456789'}
+        request.data = {'user_id': 1, 'location': 'Test', 'role': 'dater', 'phone_number': '1123456789', 'username': None}
         response = self.view(request)
-        assert response.status_code == status.HTTP_200_OK
+        assert response.status_code == status.HTTP_400_BADD_REQUEST
         mock_get_user.assert_called_once()
         mock_update_location.assert_called_once()
         
 
-
-
-
 class TestGetUser(APITestCase):
-    pass
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.url = reverse('api/get_user')
+        self.view = views.get_user
+
+    @patch("get_object_or_404")
+    @patch("helpers.initialize_serializer")
+    @patch("helpers.user_expand")
+    def good_test(self, mock_object_or_404, mock_serializer, mock_user_expand):
+        mock_object_or_404.return_value = MagicMock()
+        mock_serializer.return_value = MagicMock()
+        mock_user_expand.return_value = "Test User Data"
+        request = self.factory.post(self.url)
+        request.data["user_id"] = 1
+        request.data["is_staff"] = True
+
+        response = self.view(request)
+        assert response.status_code == status.HTTP_200_OK
+        mock_object_or_404.assert_called_once()
+        mock_serializer.assert_called_once()
+        mock_user_expand.assert_called_once()
+    
+    @patch("get_object_or_404")
+    @patch("helpers.initialize_serializer")
+    @patch("helpers.user_expand")
+    def bad_test(self, mock_object_or_404, mock_serializer, mock_user_expand):
+        mock_object_or_404.return_value = MagicMock()
+        mock_serializer.return_value = MagicMock()
+        mock_user_expand.return_value = "Test User Data"
+        request = self.factory.post(self.url)
+        request.data["user_id"] = 1
+        request.data["is_staff"] = False
+
+        response = self.view(request)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        mock_object_or_404.assert_called_once()
+        mock_serializer.assert_not_called()
+        mock_user_expand.assert_not_called()
 
 
 class TestUserExpand(APITestCase):
-    pass
+    @patch("UserSerializer")
+    def good_test(self, mock_serializer):
+        mock_serializer.return_value = MagicMock()
+        user = MagicMock()
+        serializer = MagicMock()
+        user.role = User.Role.MANAGER
+        serializer.data = {'test': 'test'}
+        return_data = helpers.user_expand(user, serializer)
+        assert return_data['user'] == {'test': 'test'}
+        assert return_data['user']['password'] == None
+        
+        user.role = User.Role.CUPID
+        return_data = helpers.user_expand(user, serializer)
+        assert return_data['user'] == {'test': 'test'}
+        assert return_data['user']['password'] == None
 
+    @patch("UserSerializer")
+    def bad_test(self, mock_serializer):
+        mock_serializer.return_value = MagicMock
+        user = MagicMock()
+        serializer = MagicMock()
+        
 
 class TestDeleteUser(APITestCase):
-    pass
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.url = reverse('api/delete_user')
+        self.view = views.delete_user
+
+    @patch("get_object_or_404")
+    @patch("User.delete")
+    def good_test(self, mock_object_or_404, mock_delete):
+        request = self.factory.post(self.url)
+        request.data["user_id"] = 1
+        request.data["is_staff"] = True
+        mock_object_or_404.return_value = MagicMock()
+        mock_delete.return_value = True
+
+        response = self.view(request)
+        assert response.status_code == status.HTTP_200_OK
+        mock_object_or_404.assert_called_once()
+        mock_delete.assert_called_once()
+
+    @patch("get_object_or_404")
+    @patch("User.delete")
+    def bad_test(self, mock_object_or_404, mock_delete):
+        request = self.factory.post(self.url)
+        request.data["user_id"] = 1
+        request.data["is_staff"] = False
+        mock_object_or_404.return_value = MagicMock()
+        mock_delete.return_value = False
+
+        response = self.view(request)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        mock_object_or_404.assert_not_called()
+        mock_delete.assert_not_called()
+
