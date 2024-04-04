83b2db717bab826130226c213614b43c46dd7499
a02242376 <a02242376@aggies.usu.edu>
2024-04-04 14:21:08 -0600

    Able to pull data from backend on CupidHome

--- Code/client/src/CupidVues/CupidHome.vue
+++ Code/client/src/CupidVues/CupidHome.vue
@@ -1,12 +1,7 @@
 <script setup>
     import { ref, onMounted } from 'vue';
     import { makeRequest } from '../utils/make_request';
-    import router from '../router/index'
-    const gigs = ref([]);
-    const numOfGigs = 10;
-
-    onMounted(getGigs);
-
+    
     const user_id  = parseInt(window.location.hash.split('/')[3]) //Gets the id from the router
     // Open and closes drawer w/ shorthand
     function openDrawer() {
@@ -24,45 +19,45 @@
         router.push('/')
     }
 
+    let gigs = ref('');
+    const gigCount = 10;
+    
     // Get gigs function
     async function getGigs() {
-        const results = await makeRequest(`/api/gig/${user_id}/${numOfGigs}`); 
-        gigs.value = results.gigs; 
+        gigs.value = await makeRequest(`api/gig/${user_id}/${gigCount}`)
+        console.log(gigs.value);
+        gigs.value.forEach(element => {
+            console.log(element.id)
+        });
     }
-
+    
     // Accept gig function 
     async function acceptgig(gigId) {
         await makeRequest(`/api/gig/accept/`, 'post', {
             id: gigId
         });
     }
-
+    
     // Drop gig function
     async function dropgig(gigId) {
         await makeRequest(`/api/gig/drop/`, 'post', {
             id: gigId
         })
     }
-
-    function naviProf() {
-        router.push({ name: 'CupidDetails', params: {id: user_id} })
-    }
-
+    
+    onMounted(getGigs);
 </script>
 
 <template>
     <nav class="nav homenav">
         <button @click="openDrawer" class="icon-button">
-            <span class="material-symbols-outlined icon">menu</span>
+            <img :src="'/get_menu/'" alt="Menu Open icon" class="icon">
         </button>
         <!-- This will be the profile picture when setup -->
-        <button class="icon-button" @click="naviProf">
-            <span class="material-symbols-outlined icon">account_circle</span>
-        </button>
+        <img :src="'/get_temp_pfp/'" alt="Profile Picture" class="icon">
         <div id="navbar" class="navbar">
             <router-link class="link" :to="{name: 'CupidHome', params: {id: user_id}}"> Home </router-link>
             <router-link class="link" :to="{name: 'CupidDetails', params: {id: user_id}}"> Profile </router-link>
-            <router-link class="link" :to="{name: 'CupidFeedback', params: {id: user_id}}"> Feedback </router-link>
             <router-link class="link" :to="{name: 'GigDetails', params: {id: user_id}}"> Gig Details </router-link>
             <router-link class="link" :to="{name: 'GigComplete', params: {id: user_id}}"> Check Completed </router-link>
             <button class="logout" @click="logout"> Logout </button>
@@ -73,6 +68,7 @@
         <h1><br/>You are in CupidHome.vue</h1>
         <!-- Clicking on this gig item, reroute to GigDetails page -->
         <div v-for="gig of gigs" :key="gig.id">
+            <h1>YEET!!!!!!!!!!!!!</h1>
             <div :class="{ 'active': gig.status, 'inactive': !gig.status }" >
                 <router-link class="link" :to="{name: 'GigDetails', params: {id: gig.id}}">{{ gig.quest.pickup_location }}</router-link>
             </div>
